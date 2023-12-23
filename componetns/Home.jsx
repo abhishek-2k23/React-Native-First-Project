e51adec1f07ae0381be3adfbdc18162c26a6940d
abhishek-2k23 <kumarabhishek00090@gmail.com>
2023-12-24 02:03:52 +0530

    Basic Home with user name

--- componetns/Home.jsx
+++ componetns/Home.jsx
@@ -1,10 +1,28 @@
-import { Text, View } from "react-native"
+/* eslint-disable prettier/prettier */
+import {StyleSheet, Text, View} from 'react-native';
 
-const Home = () => {
-    return (
-        <View>
-            <Text>Home Page</Text>
-        </View>
-    )
-}
+const Home = ({route}) => {
+  const {user} = route.params;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textStyle}>Hi, {user?.firstName}</Text>
+      <Text style={styles.textStyle}>Welcome to Home</Text>
+      
+    </View>
+  );
+};
 export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textStyle: {
+    fontSize:20,
+    fontWeight:'bold',
+    color: 'black',
+    margin:1,
+  }
+});
