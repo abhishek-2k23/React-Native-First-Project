78e68b56dc142ce02e13ceb7c0bef0c5aa1e26ef
abhishek-2k23 <kumarabhishek00090@gmail.com>
2023-12-23 22:54:31 +0530

    initial commit

---
+++ componetns/Register.jsx
@@ -0,0 +1,10 @@
+import {View, Text} from 'react-native';
+const Register = () => {
+    return (
+        <View>
+            <Text>Register Page</Text>
+        </View>
+    )
+}
+
+export default Register;
