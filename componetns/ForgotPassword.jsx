32be3e7cc1d37b31a4f6b53d2d9cfa38f546f4e9
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-01-07 02:59:29 +0530

    added forgot password basic component

---
+++ componetns/ForgotPassword.jsx
@@ -0,0 +1,20 @@
+import {View, Text, StyleSheet} from 'react-native';
+import React from 'react';
+
+const ForgotPassword = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={{fontSize: 30}}>Under Development</Text>
+    </View>
+  );
+};
+
+export default ForgotPassword;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
