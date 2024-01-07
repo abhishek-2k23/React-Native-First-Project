95d07473926a1b3308017af94eb183c14c0ddb6e
abhishek-2k23 <kumarabhishek00090@gmail.com>
2024-01-07 19:26:54 +0530

    toast parent component added

--- App.jsx
+++ App.jsx
@@ -3,18 +3,25 @@ import {NavigationContainer} from '@react-navigation/native';
 import Home from './componetns/Home';
 import Login from './componetns/Login';
 import Register from './componetns/Register';
+import ForgotPassword from './componetns/ForgotPassword';
+import Toast from 'react-native-toast-message';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Register" component={Register} />
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Register" component={Register} />
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+        </Stack.Navigator>
+      </NavigationContainer>
+
+      <Toast />
+    </>
   );
 };
 export default App;
